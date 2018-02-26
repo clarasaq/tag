18a0ca9208bd47f204d8cfc28ecbecdc0146a425
Clara <clarasaquero8@gmail.com>
2018-02-26 16:49:59 +0100

    Revert "cosiquis más"
    This reverts commit a29c192a829633ae988900d34e7e3fee176d114e.

--- Main.js
+++ Main.js
@@ -12,12 +12,15 @@ class Main {
     var TraslaLuz = new TNodo("TraslaLuz", RotaLuz);
     var NLuz = new TNodo("NLuz", TraslaLuz);
 
-    //---- Añadir las entidades a los nodos ----//
-    var TransfRotaLuz = new TTransform("Rotaluz_matrix");
-    var TransfRotaCam = new TTransform("Rotacam_matrix");
-    var TransfRotaCoche = new TTransform("Rotacoche_matrix");
+    var TraslaCoche = new TNodo("TraslaCoche", RotaCoche);
+    var NMall = new TNodo("NMall", TraslaCoche);
 
-    var TransfTraslaLuz = new TTransform("Traslaluz_matrix");
+    //---- Añadir las entidades a los nodos ----//
+    var TransfRotaLuz = new TTransform("Rotaluz_matrix", 1, 2, 3, 4, 15, 6, 7, 8, 19, 10, 11, 12, 16, 14, 15, 16);
+    var TransfRotaCam = new TTransform("Rotacam_matrix", 1, 2, 3, 14, 5, 6, 7, 18, 9, 10, 11, 12, 13, 14, 8, 16);
+    var TransfRotaCoche = new TTransform("Rotacoche_matrix", 1, 2, 3, 11, 5, 6, 7, 10, 9, 10, 11, 22, 13, 14, 25, 16);
+
+    var TransfTraslaLuz = new TTransform("Traslaluz_matrix", 1, 2, 5, 4, 5, 6, 7, 18, 9, 10, 11, 12, 23, 14, 15, 16);
 
     var EntLuz = new TLuz("NLuz");
 
@@ -39,7 +42,7 @@ class Main {
     var gestorRecursos = new TGestorRecursos();
 
     //---- Recurso ----//
-    var recursoMalla = gestorRecursos.getRecurso('1.png', 'malla');
+    var recursoMalla = gestorRecursos.getRecurso('cube.json', 'malla');
 
     //---- Recurso Malla ----//
     recursoMalla.draw();
