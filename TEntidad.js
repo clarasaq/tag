f8c6de3f9a06c4fd0c33664cc2b60f6530857cf3
Clara <clarasaquero8@gmail.com>
2018-02-26 16:50:05 +0100

    Revert "cosiquis a ver si no la lio"
    This reverts commit abb398b6f3ca6e5eed65b05a8e53d57391c8931c.

--- TEntidad.js
+++ TEntidad.js
@@ -3,9 +3,10 @@ class TEntidad {
   //clase virtual
   constructor(){
     this.stack = [];
-    this.modelMatrix = mat4.create();
+    this.modelMatriz = mat4.create();
     this.viewMatrix = mat4.create();
     this.projectionMatrix = mat4.create();
+
   }
 
   beginDraw(){}
