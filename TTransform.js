abb398b6f3ca6e5eed65b05a8e53d57391c8931c
Clara <clarasaquero8@gmail.com>
2018-02-26 16:48:17 +0100

    cosiquis a ver si no la lio

--- TTransform.js
+++ TTransform.js
@@ -1,33 +1,26 @@
 class TTransform extends TEntidad{
 
   //---- construye una matriz identidad ----//
-  // constructor(id) {
-  //   super();
-  //   this.matrix_id = mat4.create();
-  //   //Matriz 4x4 resultante:
-  //   // 1 0 0 0
-  //   // 0 1 0 0
-  //   // 0 0 1 0
-  //   // 0 0 0 1
-  //   this.id = id;
-  // }
-
-//---- construye una matriz con los valores dados ----//
-  constructor(id, m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33) {
+  constructor(id) {
     super();
-
-    this.matrix = mat4.fromValues(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33);
-    this.matrix_out = mat4.create();
     this.id = id;
   }
 
+  // constructor(id, m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33) {
+  //   super();
+  //
+  //   this.matrix = mat4.fromValues(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33);
+  //   this.matrix_out = mat4.create();
+  //   this.id = id;
+  // }
+
   getMatrix(){
-    return this.matrix;
+    return this.modelMatrix;
   }
 
-  getMatrix_out(){
-    return this.matrix_out;
-  }
+  // getMatrix_out(){
+  //   return this.matrix_out;
+  // }
 
   getId(){
     return this.id;
