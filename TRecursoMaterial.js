cf88ed39ee854d0259b751eda0a15e07ab8b8baf
Anabel Diez <anabel1996.ad@gmail.com>
2018-02-27 18:57:39 +0100

    Tmaterial

--- TRecursoMaterial.js
+++ TRecursoMaterial.js
@@ -1,3 +1,5 @@
 class TRecursoMaterial extends TRecurso{
   constructor(){}
+  //buscar estructura archivos .mtl leerlo y cargarlo como hacemos con la TRecursoMalla
+  //almacenar color difuso, color especular, etc
 }
